4ef5064a4c0ac3c15987acedb7c2bb17577767f3
Angela Xu <angela.xu.dev@gmail.com>
2023-02-18 11:37:53 -0500

    google calendar API setup. Problem with circular import

---
+++ weather-wear/src/backend/calendar.py
@@ -0,0 +1,11 @@
+from pprint import pprint 
+from google_apis import create_service
+
+CLIENT_SECRET_FILE = 'client_secret.json'
+API_NAME = 'calendar'
+API_VERSION = 'v3'
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+service = create_service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
+print(dir(service))
