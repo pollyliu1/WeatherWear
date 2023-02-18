0cb7152828bd2552a71640aa7a39cb9874f4d15b
Angela Xu <angela.xu.dev@gmail.com>
2023-02-18 14:17:00 -0500

    destroyed

--- weather-wear/src/pages/_app.js
+++ weather-wear/src/pages/_app.js
@@ -1,5 +1,5 @@
 import '@/styles/globals.css'
-import Navbar from 'components/Navbar'
+import Navbar from '@/components/Navbar'
 
 export default function App({ Component, pageProps }) {
   return <>
