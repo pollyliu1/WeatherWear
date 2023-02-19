6d730821777b158394e8b2c2d7a679de2d77cabf
Jessica <jessica2003mail@gmail.com>
2023-02-18 23:54:05 -0500

    Add content to info

--- weather-wear/src/pages/info.js
+++ weather-wear/src/pages/info.js
@@ -14,14 +14,26 @@ export default function Info() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.icon" />
       </Head>
-      <div className="about-description">
-      <h2>
-          get started with WeatherWear
-        </h2>
-        <h4>
-          an AI powered web app to assist you in dressing for the weather.
+      <div className={styles.aboutdescription}>
+        <h1 className={styles.header}>Dress for the Weather</h1>
+        <h4 className={styles.paragraph}>
+          Have trouble deciding what to wear in the morning? <br/>
+          Feeling stressed about keeping up with fashion trends while being comfortable? <br/>
+          
+          Here are some tips:<br/><br/>
+
+          1. Layering is key. Enjoy varying temperatures throughout the day by adding or removing layers of clothing
+          to stay comfortable.<br/><br/>
+
+          2. Watch for wind. Wind chill can dramatically change how warm you feel, especially during colder seasons.<br/><br/>
+
+          3. Explore humidity. Higher amounts of water vapour in the air can make you feel warmer, while lower amounts
+          can make you feel cooler.<br/><br/>
+
+          4. Bring a jacket. Although you might want to dress for the season, temperatures can vary day to day, and it 
+          is often a good idea to bring a jacket. 
         </h4>  
-      </div>
+    </div>
       
     </>
   )
